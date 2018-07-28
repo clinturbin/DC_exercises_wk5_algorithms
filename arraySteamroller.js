00d49593c13e2a83158d322dd8de8074f8834cf8
clinturbin <clinturbin@gmail.com>
2018-07-28 10:33:53 -0400

    Wrapped code into Immediately Invoked Function Expression.

--- arraySteamroller.js
+++ arraySteamroller.js
@@ -2,51 +2,16 @@
 //                Array Steamroller
 //================================================
 
-// var startArray = [1, [2], [3, [[4]]]];
-var startArray = [1, [2], [3, [[4]]], [[5], [6, 7]]];
-var newArray = [];
+(function () {
+    var startArray = [1, [2], [3, [[4]]]];
+    var newArray = [];
 
-
-var steamrollArray = function (arr) {
-    arr.forEach(function (item) {
-        Array.isArray(item) ? steamrollArray(item) : newArray.push(item);
-    });
-};
-
-steamrollArray(startArray);
-console.log(newArray);
-
-
-
-
-// var steamrollArray = function (arr) {
-//     for (var item of arr) {
-//         if (Array.isArray(item)) {
-//             steamrollArray(item);
-//         }
-//         else {
-//             newArray.push(item);
-//         }
-//     }
-//     return;
-// };
-
-// steamrollArray(startArray);
-// console.log(newArray);
-
-
-
-// var steamrollArray = function (arr) {
-//     arr.forEach(function (item) {
-//         if (Array.isArray(item)) {
-//             steamrollArray(item);
-//         }
-//         else {
-//             newArray.push(item);
-//         }
-//     });
-//     return newArray;
-// };
-
-// steamrollArray(startArray);
-// console.log(newArray);
+    var steamrollArray = function (arr) {
+        arr.forEach(function (item) {
+            Array.isArray(item) ? steamrollArray(item) : newArray.push(item);
+        });
+    };
+    
+    steamrollArray(startArray);
+    console.log(newArray);
+})();
